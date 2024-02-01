761288fc44063323167366dd43bb89bb673de07a
YES!HYUNGSEOK <eric.nohhyungseok@gmail.com>
2024-02-01 19:06:10 +0900

    [level 1] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,7 @@
+def solution(cards1, cards2, goal):
+    i, j = 0, 0
+    for g in goal:
+        if i < len(cards1) and g == cards1[i]: i += 1
+        elif j < len(cards2) and g == cards2[j]: j += 1
+        else: return "No"
+    return "Yes"
